a4a367f8c260de3bb556e6a1b4e5d0d1bdc4624e
nhthuong2208 <nguyenhoaithuong5690@gmail.com>
2021-10-29 00:29:17 +0700

    initial project

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar/Navbar";
+import Home from "./Components/Pages/Home";
+import Demo from "./Components/Pages/SignIn";
+import "./App.css";
+import ViewMenu from "./Components/Pages/ViewMenu";
+import Cart from "./Components/Pages/Cart";
+import AboutUs from "./Components/Pages/AboutUs";
+
+function App() {
+  return (
+    <div className="container">
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/view-menu" exact component={ViewMenu} />
+          <Route path="/cart" exact component={Cart} />
+          <Route path="/about-us" exact component={AboutUs} />
+          <Route path="/demo" exact component={Demo} />
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
